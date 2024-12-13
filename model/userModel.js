fde961fd9360b5c30ee55c9cd739ad4806cadacc
subin jose <subinjose515@gmail.com>
2024-12-13 10:37:16 +0530

    second  commit

--- model/userModel.js
+++ model/userModel.js
@@ -15,4 +15,4 @@ const userSchema = new mongoose.Schema({
     },
 })
 
-export default mongoose.model("users",userSchema);
+export default mongoose.model("createdusers",userSchema);
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,7 +2,7 @@ import express from "express"
 import { fetch,create,update, deleteUser } from "../controller/userController.js"
 
 const route = express.Router();
-route.get("/getAllUsers",fetch);
+route.get("/viewcreatedusers",fetch);
 route.post("/create",create);
 route.put("/update/:id",update);
 route.delete("/delete/:id",deleteUser);
